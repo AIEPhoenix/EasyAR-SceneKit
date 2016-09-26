473fc1ebf4fdb94642a7eba32890100d282ce6bf
XHF <XHF@163.com>
2016-09-26 23:01:13 +0800

    绕x轴旋转,固定太阳位置

--- HelloARVideo/helloarvideo/ViewController.m
+++ HelloARVideo/helloarvideo/ViewController.m
@@ -12,9 +12,8 @@
 @property(nonatomic)int type;
 @property (assign, nonatomic) SCNMatrix4 projection4Matrix;
 @property (assign, nonatomic) SCNMatrix4 cameraview4Matrix;
+@property (strong, nonatomic) CADisplayLink *displayLink;
 
-
-@property(strong,nonatomic)SCNNode *boxNode;
 @end
 
 @implementation ViewController
@@ -26,10 +25,11 @@
     [self.glView setOrientation:self.interfaceOrientation];
     
     [self initScene];
-//    [self setupCube];
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(4 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        
-        [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(changeProjection4Matrix) userInfo:nil repeats:YES];
+
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        self.displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(changeProjection4Matrix)];
+        [self.displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+
     });
     
     
@@ -61,10 +61,8 @@
     NSLog(@"cameraview4Matrix--\n%lf,%lf,%lf,%lf,\n%lf,%lf,%lf,%lf,\n%lf,%lf,%lf,%lf,\n%lf,%lf,%lf,%lf\n",c.m11,c.m12,c.m13,c.m14,c.m21,c.m22,c.m23,c.m24,c.m31,c.m32,c.m33,c.m34,c.m41,c.m42,c.m43,c.m44);
 //    NSLog(@"projection4Matrix--\n%lf,%lf,%lf,%lf,\n%lf,%lf,%lf,%lf,\n%lf,%lf,%lf,%lf,\n%lf,%lf,%lf,%lf\n",p.m11,p.m12,p.m13,p.m14,p.m21,p.m22,p.m23,p.m24,p.m31,p.m32,p.m33,p.m34,p.m41,p.m42,p.m43,p.m44);
     
-//    self.boxNode.transform = c;
     self.sunNode.transform = c;
-//    self.boxNode.transform = [self.boxNode convertTransform:c toNode:self.boxNode];
-
+    self.sunNode.rotation = SCNVector4Make(1, 0, 0, M_PI_2);
 }
 
 /**
@@ -81,13 +79,13 @@
     
     // check that we clicked on at least one object
     if([hitResults count] > 0){
-        // retrieved the first clicked object
+        // 拿到点击处最近的对象
         SCNHitTestResult *result = [hitResults objectAtIndex:0];
         
-        // get its material
+        // 拿到对象的材质
         SCNMaterial *material = result.node.geometry.firstMaterial;
         
-        // highlight it
+        // 高亮,延时0.5s
         [SCNTransaction begin];
         [SCNTransaction setAnimationDuration:0.5];
         
@@ -108,32 +106,6 @@
 }
 
 #pragma mark 组件初始化方法
-- (void)setupCube {
-    // 实例化一个空的SCNScene类，接下来要用它做更多的事
-    SCNScene *scene = [[SCNScene alloc] init];
-    // 定义一个SCNBox类的几何实例然后创建盒子，并将其作为根节点的子节点，根节点就是scene
-    SCNBox *boxGeometry = [SCNBox boxWithWidth:10.0 height:10.0 length:10.0 chamferRadius:1.0];
-    SCNNode *boxNode = [SCNNode nodeWithGeometry:boxGeometry];
-    self.boxNode = boxNode;
-    [scene.rootNode addChildNode:boxNode];
-    // 将场景放进sceneView中显示
-    self.scnView.scene = scene;
-    //添加灯泡效果
-    self.scnView.autoenablesDefaultLighting = YES;
-    
-    //添加环境光，灰绿光
-    SCNNode *ambientLightNode = [[SCNNode alloc] init];
-    ambientLightNode.light = [[SCNLight alloc] init];
-    ambientLightNode.light.type = SCNLightTypeAmbient;
-    ambientLightNode.light.color = [UIColor colorWithRed:0.1 green:0.2 blue:0.1 alpha:1];
-    [scene.rootNode addChildNode:ambientLightNode];
-//    //修改摄像机位置
-    SCNNode *cameraNode = [[SCNNode alloc] init];
-    cameraNode.camera = [[SCNCamera alloc] init];
-    cameraNode.position = SCNVector3Make(0, 0, 50);
-    [scene.rootNode addChildNode:cameraNode];
-    cameraNode.rotation =  SCNVector4Make(0, 0, 1,-M_PI_2);
-}
 
 -(void)initScene{
     
@@ -146,7 +118,7 @@
     [scene.rootNode addChildNode:cameraNode];
     
     // 放置camera
-    cameraNode.position = SCNVector3Make(0,0,20);
+    cameraNode.position = SCNVector3Make(0,0,0);
     cameraNode.camera.zFar = 200;
     cameraNode.rotation =  SCNVector4Make(0, 0, 1,-M_PI_2);
         
@@ -190,12 +162,13 @@
     
     //太阳节点位置
     _sunNode.position = SCNVector3Make(0, 0, 0);
+   
     //太阳节点添加到根节点
     [_scnView.scene.rootNode addChildNode:_sunNode];
     
     
     
-    // Add materials to the planets
+    // 添加各星球材质
     _earthNode.geometry.firstMaterial.diffuse.contents = @"art.scnassets/earth/earth-diffuse-mini.jpg";
     _earthNode.geometry.firstMaterial.emission.contents = @"art.scnassets/earth/earth-emissive-mini.jpg";
     _earthNode.geometry.firstMaterial.specular.contents = @"art.scnassets/earth/earth-specular-mini.jpg";
@@ -228,6 +201,7 @@
     
     // We will turn off all the lights in the scene and add a new light
     // to give the impression that the Sun lights the scene
+    // 关闭场景中所有光源并添加一个新光源,来模拟场景中的太阳光
     SCNNode *lightNode = [SCNNode node];
     lightNode.light = [SCNLight light];
     lightNode.light.color = [UIColor blackColor]; // initially switched off
@@ -235,10 +209,11 @@
     [_sunNode addChildNode:lightNode];
     
     // Configure attenuation distances because we don't want to light the floor
+    // 配置光源衰减距离
     lightNode.light.attenuationEndDistance = 20.0;
     lightNode.light.attenuationStartDistance = 19.5;
     
-    // Animation
+    // 动画
     [SCNTransaction begin];
     [SCNTransaction setAnimationDuration:1];
     {
@@ -269,7 +244,7 @@
     //太阳光环节点
     _sunHaloNode = [SCNNode node];
     _sunHaloNode.geometry = [SCNPlane planeWithWidth:25 height:25];
-    _sunHaloNode.rotation = SCNVector4Make(1, 0, 0, 0 * M_PI / 180.0);
+    _sunHaloNode.rotation = SCNVector4Make(1, 0, 0,  0.5* M_PI);
     _sunHaloNode.geometry.firstMaterial.diffuse.contents = @"art.scnassets/earth/sun-halo.png";
     _sunHaloNode.geometry.firstMaterial.lightingModelName = SCNLightingModelConstant; // no lighting
     _sunHaloNode.geometry.firstMaterial.writesToDepthBuffer = NO; // do not write to depth
@@ -344,7 +319,7 @@
         [_sunNode addChildNode:_earthGroupNode];
         [self mathRoation];
     }
-    
+
     //太阳的熔岩效果
     [self addAnimationToSun];
 }
