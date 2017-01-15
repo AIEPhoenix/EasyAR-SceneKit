a73f9f425bf5eca304403fbe032a7927881c055f
CoderHF <517807635@qq.com>
2017-01-15 18:10:38 +0800

    整理代码

--- HelloARVideo/helloarvideo/ViewController.m
+++ HelloARVideo/helloarvideo/ViewController.m
@@ -27,14 +27,6 @@
     
     [self initScene];
     self.sunNode.hidden = YES;
-    
-//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-//        self.displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(changeProjection4Matrix)];
-//        [self.displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-//
-//    });
-    
-    
 }
 - (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
